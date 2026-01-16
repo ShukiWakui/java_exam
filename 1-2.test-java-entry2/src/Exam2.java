419de9fe31acb99b19bb09439b5354472b004d4d
ShukiWakui <shuki.wakui@rakus-partners.co.jp>
2026-01-16 11:10:40 +0900

    プログラミング入門２

---
+++ 1-2.test-java-entry2/src/Exam2.java
@@ -0,0 +1,9 @@
+public class Exam2 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10) {
+            System.out.println("和久井柊希");
+            i++;
+        }
+    }
+}
