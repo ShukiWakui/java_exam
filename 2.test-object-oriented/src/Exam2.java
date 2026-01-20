a7bc14576ebe4c24f661944619c4ba31cd9901c3
ShukiWakui <shuki.wakui@rakus-partners.co.jp>
2026-01-20 09:45:43 +0900

    オブジェクト指向入門

---
+++ 2.test-object-oriented/src/Exam2.java
@@ -0,0 +1,16 @@
+public class Exam2 {
+    public static void main(String[] args) {
+        String[] week = new String[7];
+        week[0] = "月";
+        week[1] = "火";
+        week[2] = "水";
+        week[3] = "木";
+        week[4] = "金";
+        week[5] = "土";
+        week[6] = "日";
+
+        for (String day: week) {
+            System.out.println(day);
+        }
+    }
+}
