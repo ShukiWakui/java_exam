b693e6b65ceab124f2199a47be1f7fc3698bfb07
ShukiWakui <shuki.wakui@rakus-partners.co.jp>
2026-01-23 09:50:17 +0900

    Java応用１

---
+++ 4-1.test-java-technique/src/Exam1.java
@@ -0,0 +1,12 @@
+public class Exam1 {
+    /**
+     * 自分の名前の文字数を表示
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        String name = "わくいしゅうき";
+        System.out.println(name + "の文字数は" + name.length() + "文字です");
+    }
+    
+}
---
+++ 4-1.test-java-technique/src/Exam4.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+
+public class Exam4 {
+    public static void main(String[] args) {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        // int nano = localDateTime.getNano();
+        System.out.println(localDateTime);
+    }
+}
