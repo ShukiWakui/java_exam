680d437042bbe5dc09d19b780894acf4117ae70c
ShukiWakui <shuki.wakui@rakus-partners.co.jp>
2026-02-18 10:01:32 +0900

    Thymeleaf

---
+++ test-spring-thymeleaf-shukiwakui/src/main/java/com/example/test_spring_thymeleaf_shukiwakui/TestSpringThymeleafShukiwakuiApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_spring_thymeleaf_shukiwakui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringThymeleafShukiwakuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringThymeleafShukiwakuiApplication.class, args);
+	}
+
+}
---
+++ test-spring-thymeleaf-shukiwakui/src/test/java/com/example/test_spring_thymeleaf_shukiwakui/TestSpringThymeleafShukiwakuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_spring_thymeleaf_shukiwakui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringThymeleafShukiwakuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
