5b718225b2b206003ac24fc22dde4bc352610c9b
ShukiWakui <shuki.wakui@rakus-partners.co.jp>
2026-03-02 11:41:06 +0900

    ロジック & Spring テスト

---
+++ spring-exam-ShukiWakui/exam-web-spring-thymeleaf/src/main/java/com/example/q7/CalcForm.java
@@ -0,0 +1,26 @@
+package com.example.q7;
+
+import jakarta.validation.constraints.Digits;
+
+public class CalcForm {
+	// @Digits(fraction = 10, integer = 10, message = "左のテキストボックスには数値を入力してください。")
+	private String left;
+	// @Digits(fraction = 10, integer = 10, message = "右のテキストボックスには数値を入力してください。")
+	private String right;
+
+	public String getLeft() {
+		return left;
+	}
+
+	public void setLeft(String left) {
+		this.left = left;
+	}
+
+	public String getRight() {
+		return right;
+	}
+
+	public void setRight(String right) {
+		this.right = right;
+	}
+}
